e81f88eee26302ad79a337edeaf32872c2cfcde0
insoo <inu0616@gmail.com>
2016-05-25 15:08:23 +0900

    Index template, view, url setting

--- gimal/blog/views.py
+++ gimal/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views import generic
+from django.views.generic import View
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return Post.objects.all()
